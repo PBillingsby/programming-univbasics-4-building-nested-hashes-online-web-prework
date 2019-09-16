93319dcbe7c97e2e08583e4b28ad099e39fa2a9e
Paul Billingsby <paulbillingsby@gmail.com>
2019-09-16 19:35:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,7 +68,7 @@ def monopoly_with_fourth_tier
         shortline_railroad: {
           "mortgage_value" => "$800"
         }
-        },
+      },
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
