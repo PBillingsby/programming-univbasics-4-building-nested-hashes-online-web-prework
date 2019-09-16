b3e900a3b7a9654364796ab80a1e80e58e84ab59
Paul Billingsby <paulbillingsby@gmail.com>
2019-09-16 19:29:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       names: {
-        suburban: "suburban",
+        reading_railroad: "Reading",
         jefferson: "jefferson",
         broad: "broad",
         overbrook: "overbrook"
